d2b2b63af470bd79843d7fdcb6ad81d7a1e3678c
Brandon <brandon@melissa-brandon.com>
2011-02-15 21:52:55 -0800

    Adding all files

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,8 @@
+class LandingController < Spree::BaseController
+ layout 'home_page'
+ 
+ def index
+  
+ end
+ 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Henna::Application.config.secret_token = '88f31ada562ec2caacae6cd9ebfec3aac805b2c035bf7826008fb50aa2120fb52bf7578558757491c5509ab8ef0a208da7bf043ab34a18c850e5a08fbd9f38a8'
