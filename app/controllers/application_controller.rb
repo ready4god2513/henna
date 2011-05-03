f834210ffa6bf64eef43cac7da48696ca1e44868
ready4god2513 <brandonh@ibethel.org>
2011-05-03 11:44:45 -0700

    Error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,14 @@ class ApplicationController < ActionController::Base
   rescue_from NoMethodError, with: :render_500
   
   def render_404
-    render template: "errors/routing", status: 404
+    render layout: "spree_application", template: "errors/routing", status: 404
   end
   
   def render_500
-    render template: "errors/500", status: 500
+    render layout: "spree_application", template: "errors/500", status: 500
   end
 
   def render_record_not_found
-    render template: "errors/record_not_found", status: 404
+    render layout: "spree_application", template: "errors/record_not_found", status: 404
   end
 end
