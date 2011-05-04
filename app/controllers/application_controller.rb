07e605c82bdc38eda2f4da3166a35f3835103698
Brandon <brandon@melissa-brandon.com>
2011-05-03 21:34:01 -0700

    Catching 500 level errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
-  rescue_from AbstractController::ActionNotFound, with: :render_404
-  rescue_from NoMethodError, with: :render_500
-  
-  def render_404
-    render layout: "spree_application", template: "errors/routing", status: 404
-  end
-  
-  def render_500
-    render layout: "spree_application", template: "errors/500", status: 500
-  end
-
-  def render_record_not_found
-    render layout: "spree_application", template: "errors/record_not_found", status: 404
-  end
 end
