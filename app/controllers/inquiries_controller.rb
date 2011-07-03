ac2348120feb74e629132ce60417dacc1f90367c
Brandon Hansen <brandonh@ibethel.org>
2011-07-03 13:35:50 -0700

    Fixing link to contact form

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -2,7 +2,7 @@ class InquiriesController < Spree::BaseController
   resource_controller
   
   def index
-    #redirect_to(new_inquiry_url) unless params[:inquiry]
+    # redirect_to(new_inquiry_url) unless params[:inquiry]
     build_object
     load_object
     render :action => :new
