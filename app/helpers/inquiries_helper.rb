38b732b6695cf204880cdbf3ce48ffdfb1dbff01
Brandon Hansen <brandonh@ibethel.org>
2011-07-03 17:07:33 -0700

    Fixing inquiries

--- app/helpers/inquiries_helper.rb
+++ app/helpers/inquiries_helper.rb
@@ -1,11 +1,5 @@
 module InquiriesHelper
-  def inquiry_types
-    %w[
-      General_Inquiry
-      Order_Inquiry
-      Order_Change
-      Vendor_Inquiry
-      Feedback
-    ].map {|x| x.sub(/\_/, ' ') }
-  end
+  def translate_types
+      INQUIRY_TYPES.collect { |i| t(i) }
+    end
 end
--- app/models/app_configuration_decorator.rb
+++
@@ -1,5 +0,0 @@
-AppConfiguration.class_eval do
-    preference :contact_us_ask_for_order_number, :boolean, :default => true
-    preference :contact_us_ask_for_phone, :boolean, :default => true
-    preference :contact_us_inquiry_types, :string, :default => 'General Information,Order Status,Feedback,Feature Request,Problem With Site'
-end
